47654171e34426ca72d26b43e36c301825fc681c
Jonathan Bell <jon@jonbell.net>
2022-10-24 22:03:13 -0400

    LIKERT SCALES!!!

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM rocker/shiny-verse:4
 
 RUN Rscript -e "install.packages('qualtRics')"
+RUN Rscript -e "install.packages('HH')"
 
 COPY server.R /srv/shiny-server/
 COPY ui.R /srv/shiny-server/
--- server.R
+++ server.R
@@ -18,100 +18,122 @@ qualtrics_api_credentials(Sys.getenv(c("QUALTRICS_API_KEY")), Sys.getenv(c("QUAL
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    tcrs <- reactive({
-        fetch_survey(
-            surveyID = Sys.getenv(c("SURVEY_ID")), # Get from logging into qualtrics and clicking on "Qualtrics IDs" at the top
-            verbose = TRUE,
-            force_request = ifelse(input$forceReload > 0, TRUE, FALSE)
-        )
-    })
-    colMap <- reactive({
-      extract_colmap(tcrs())
-    })
+  tcrs <- reactive({
+    fetch_survey(
+      surveyID = Sys.getenv(c("SURVEY_ID")), # Get from logging into qualtrics and clicking on "Qualtrics IDs" at the top
+      verbose = TRUE,
+      force_request = ifelse(input$forceReload > 0, TRUE, FALSE)
+    )
+  })
+  colMap <- reactive({
+    extract_colmap(tcrs())
+  })
 
-    questions <- reactive({
-        survey_questions(Sys.getenv(c("SURVEY_ID")))
-    })
-    roster <- reactive({
-        fetch_mailinglist(Sys.getenv(c("MAILING_LIST_ID")))  #also from "qualtics IDs" page
-    })
-    
-    section <- reactive({
-      filter(roster(), '-' %in% input$filterBySection | section %in% input$filterBySection)
-    })
-    
-    groupResults <- reactive({
-      roster() %>%
-        left_join(tcrs(), by=c('email'='RecipientEmail')) %>%
-        filter(('-' %in% input$filterByGroup & (section %in% input$filterBySection | '-' %in% input$filterBySection)) | group %in% input$filterByGroup)
-    })
-    
-    observeEvent(input$section, {
-      updateSelectInput(inputid = "section", choices = roster() %>% filter() )
-    })
+  questions <- reactive({
+    survey_questions(Sys.getenv(c("SURVEY_ID")))
+  })
+  roster <- reactive({
+    fetch_mailinglist(Sys.getenv(c("MAILING_LIST_ID"))) # also from "qualtics IDs" page
+  })
 
-    output$howDidYouDo <- renderPlot({
-      # just the "how did you do" questions
-      qs <- c('Q4', 'Q5', 'Q7')
-      resp <- groupResults() %>% select(fullname, any_of(qs))  %>% pivot_longer(-fullname)
-      # resp
-      respG <- resp %>% filter(!is.na(value)) %>% group_by(name,value) %>% summarise(count=n()) %>% inner_join(colMap(), by=c("name"="qname"))
-      
-      ggplot(respG) + 
-        geom_bar(aes(y=str_wrap(main, 25),x=count, fill=value), position = "stack", stat='identity') +  scale_fill_brewer(palette="PRGn")+
-        theme(legend.position="bottom", axis.title.x=element_blank(), axis.title.y=element_blank())
-      
-    })
-    
-    output$sentiment <- renderPlot({
-      sentimentQs <- c('Q3', 'Q6', 'Q16', 'Q21', 'Q23', 'Q24')
-      sentiment <- groupResults()  %>% select(fullname, any_of(sentimentQs)) %>% pivot_longer(-fullname) %>% filter(!is.na(value)) 
-      sentimentG <- sentiment %>% group_by(name,value) %>% summarise(count=n()) %>% inner_join(colMap(), by=c("name"="qname"))
-      
-      ggplot(sentimentG) + 
-        geom_bar(aes(y=str_wrap(main, 25),x=count, fill=value), position = "stack", stat='identity') +  #scale_fill_brewer(palette="PRGn")+
-        theme(legend.position="bottom", axis.title.x=element_blank(), axis.title.y=element_blank())
-    })
-    output$thisWeekIHave <- renderUI({
-      # The "This week I have questions"
-      q <- c("QID1", "QID2", "QID9", "QID10","QID9_7_TEXT","Q1_7_TEXT","Q2_6_TEXT","Q10_5_TEXT")
-      qs <- colMap() %>%
-        filter(ImportId %in% q) %>%
-        select(qname)
-      thisWeekI <- groupResults() %>%
-        select(fullname, any_of(qs$qname)) %>%
-        pivot_longer(-fullname) %>%
-        inner_join(colMap(), by=c("name"="qname")) %>%
-        filter(!is.na(value) & value != "Other") %>%
-        group_by(main, fullname) %>%
-        summarise(value= paste0(value, collapse=", ")) %>% # group together by respondent
-        ungroup()%>%
-        mutate(value=paste0("<li>",fullname,": ",value,"</li>")) %>% # each respondent summarised with name then list
-        group_by(main) %>% 
-        summarise(value=paste0(value,collapse="\n")) %>% ungroup() %>%
-        mutate(summary=paste0("<h4>",main,"</h4><ul>",value,"</ul>")) %>%
-        select(summary) %>% summarise(summary=paste0(summary,collapse="\n"))
-      HTML(thisWeekI$summary)
-    })
-    output$reflection <- renderTable({
-        groupResults() %>% select(fullname,Q15)
-    })
-    output$completionInfo <- renderTable({
-      groupResults() %>% mutate(minutes=`Duration (in seconds)`/60, EndDate=format(EndDate)) %>% select(fullname, EndDate, minutes) 
-    })
-    output$filterBySection <- renderUI({
-        selectInput("filterBySection", "Section:",
-            choices = c(section="-") %>% bind_rows(roster()) %>% distinct(section) %>% select(section)
-        )
-    })
-    output$filterByDate <- renderUI({
-      selectInput("filterByDate", "Reflection Period:",
-                  choices = groupResults() %>% distinct(Q27) %>% select(Q27)
-      )
-    })
-    output$filterByTeam <- renderUI({
-        selectInput("filterByGroup", "Group:",
-            choices = c(group="-") %>% bind_rows(section()) %>% distinct(group) %>% select(group) %>% arrange(group)
-        )
-    })
+  section <- reactive({
+    filter(roster(), "-" %in% input$filterBySection | section %in% input$filterBySection)
+  })
+
+  groupResults <- reactive({
+    roster() %>%
+      left_join(tcrs(), by = c("email" = "RecipientEmail")) %>%
+      filter(("-" %in% input$filterByGroup & (section %in% input$filterBySection | "-" %in% input$filterBySection)) | group %in% input$filterByGroup)
+  })
+
+  observeEvent(input$section, {
+    updateSelectInput(inputid = "section", choices = roster() %>% filter())
+  })
+
+  output$howDidYouDo <- renderPlot({
+    # just the "how did you do" questions
+    qs <- c("Q4", "Q5", "Q7")
+    resp <- groupResults() %>%
+      select(fullname, any_of(qs)) %>%
+      pivot_longer(-fullname)
+    # resp
+    respG <- resp %>%
+      filter(!is.na(value)) %>%
+      mutate(value=fct_rev(value)) %>%
+      group_by(name, value) %>%
+      summarise(count = n()) %>%
+      inner_join(colMap(), by = c("name" = "qname")) %>%
+    select(main, value, count) %>%
+    mutate(main = str_wrap(main, 15)) %>%
+    pivot_wider(names_from=value,values_from=count, values_fill = 0)
+
+  HH::likert(main~., respG, ReferenceZero=3, main="", ylab="") 
+  })
+
+  output$sentiment <- renderPlot({
+    sentimentQs <- c("Q3", "Q6", "Q16", "Q21", "Q23", "Q24")
+
+    sentiment <- groupResults() %>%
+      select(fullname, any_of(sentimentQs)) %>%
+      pivot_longer(-fullname) %>%
+      filter(!is.na(value))
+
+    sentimentG <- sentiment %>%
+      mutate(value=fct_rev(value)) %>%
+      group_by(name, value) %>%
+      summarise(count = n()) %>%
+      ungroup() %>%
+      inner_join(colMap(), by = c("name" = "qname")) %>%
+      select(main, value, count) %>%
+      mutate(main = str_wrap(main, 25)) %>%
+      pivot_wider(names_from = value, values_from = count, values_fill = 0)
+
+    HH::likert(main ~ ., sentimentG, ReferenceZero = 3, main = "", ylab = "")
+  })
+  output$thisWeekIHave <- renderUI({
+    # The "This week I have questions"
+    q <- c("QID1", "QID2", "QID9", "QID10", "QID9_7_TEXT", "Q1_7_TEXT", "Q2_6_TEXT", "Q10_5_TEXT")
+    qs <- colMap() %>%
+      filter(ImportId %in% q) %>%
+      select(qname)
+    thisWeekI <- groupResults() %>%
+      select(fullname, any_of(qs$qname)) %>%
+      pivot_longer(-fullname) %>%
+      inner_join(colMap(), by = c("name" = "qname")) %>%
+      filter(!is.na(value) & value != "Other") %>%
+      group_by(main, fullname) %>%
+      summarise(value = paste0(value, collapse = ", ")) %>% # group together by respondent
+      ungroup() %>%
+      mutate(value = paste0("<li>", fullname, ": ", value, "</li>")) %>% # each respondent summarised with name then list
+      group_by(main) %>%
+      summarise(value = paste0(value, collapse = "\n")) %>%
+      ungroup() %>%
+      mutate(summary = paste0("<h4>", main, "</h4><ul>", value, "</ul>")) %>%
+      select(summary) %>%
+      summarise(summary = paste0(summary, collapse = "\n"))
+    HTML(thisWeekI$summary)
+  })
+  output$reflection <- renderTable({
+    groupResults() %>% select(fullname, Q15)
+  })
+  output$completionInfo <- renderTable({
+    groupResults() %>%
+      mutate(minutes = `Duration (in seconds)` / 60, EndDate = format(EndDate)) %>%
+      select(fullname, EndDate, minutes)
+  })
+  output$filterBySection <- renderUI({
+    selectInput("filterBySection", "Section:",
+      choices = c(section = "-") %>% bind_rows(roster()) %>% distinct(section) %>% select(section)
+    )
+  })
+  output$filterByDate <- renderUI({
+    selectInput("filterByDate", "Reflection Period:",
+      choices = groupResults() %>% distinct(Q27) %>% select(Q27)
+    )
+  })
+  output$filterByTeam <- renderUI({
+    selectInput("filterByGroup", "Group:",
+      choices = c(group = "-") %>% bind_rows(section()) %>% distinct(group) %>% select(group) %>% arrange(group)
+    )
+  })
 })
